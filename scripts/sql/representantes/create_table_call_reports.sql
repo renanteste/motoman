32ee99592bc868030123a9ca73879e35f101fe26
Marcelo Mendes Colato <mm.colato@gmail.com>
2025-02-10 12:17:48 -0300

    Recupera call reports

--- scripts/sql/representantes/create_table_call_reports.sql
+++ scripts/sql/representantes/create_table_call_reports.sql
@@ -22,7 +22,8 @@ CREATE TABLE call_reports (
     sub_locality VARCHAR(50),
     sub_thoroughfare VARCHAR(50),
     tipo_visita VARCHAR(50),
-    data_transmissao DATETIME
+    data_transmissao DATETIME,
+    data_erp DATETIME
 );
 CREATE UNIQUE INDEX idx_call_reports_id ON call_reports (id);
 CREATE INDEX idx_call_reports_cnpj_representante_cnpj_prospect_data_visita 
