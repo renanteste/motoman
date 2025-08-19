bfafb5362d351f9f073b2602be6a354d71160c52
Marcelo Mendes Colato <mm.colato@gmail.com>
2025-08-19 10:31:32 -0300

    Criado endpoint xml_to_json

--- src/mrb/common/routers_compartilhados.py
+++ src/mrb/common/routers_compartilhados.py
@@ -7,6 +7,7 @@ from src.mrb.common.security.criptografia import (
 )
 from src.mrb.common.security.opcoes_acesso import lista_opcoes_portal_router
 from src.mrb.common.security.auth_service import auth_router
+from src.mrb.common.utilities.xml_to_json import xml_to_json_router
 
 routers_compartilhados = APIRouter()
 
@@ -15,3 +16,4 @@ routers_compartilhados.include_router(criptografia_router)
 routers_compartilhados.include_router(descriptografia_router)
 routers_compartilhados.include_router(lista_opcoes_portal_router)
 routers_compartilhados.include_router(donwload_arquivos)
+routers_compartilhados.include_router(xml_to_json_router)
